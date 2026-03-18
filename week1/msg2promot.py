138d341a0745104126de1fa7c07f228ad6205ede
gcw_gG1ZDZq9 <gcw_gG1ZDZq9@noreply.gitcode.com>
2026-03-18 09:14:53 +0800

    add show rendered promot py code

---
+++ week1/msg2promot.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
+rendered_prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+print(rendered_prompt)
